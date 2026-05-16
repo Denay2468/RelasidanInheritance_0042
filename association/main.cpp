2dec38ee58f08298b072dbaff25ed0ebd717f8f8
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:29:07 +0630

    create associate class for patient and code

--- association/main.cpp
+++ association/main.cpp
@@ -4,3 +4,17 @@
 using namespace std;
 
 class doctor;
+
+class patient { 
+public:
+    std::string name;
+    std::vector<doctor*> doctor_list;
+    patient(std::string pName) :name(pName) {
+        std::cout << "Patient \"" << name << "\" exists\n";
+    }
+    ~patient() {
+        std::cout << "Patient \"" << name << "\" does not exist\n";
+    }
+    void addDoctor(doctor*);
+    void printDoctors();
+};
