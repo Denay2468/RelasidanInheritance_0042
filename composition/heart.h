f4b97b82b44287017b3a4060627bfa8efd0cee12
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:41:42 +0630

    create heart class and construtor , deconstructor

--- composition/heart.h
+++ composition/heart.h
@@ -1,3 +1,14 @@
 #ifndef HEART_H
 #define HEART_H
 #include <iostream>
+
+class heart {
+public:
+    heart() {
+        std::cout << "heart turned on\n";
+    }
+    ~heart() {
+        std::cout << "heart turned off\n";
+    }
+};
+#endif
