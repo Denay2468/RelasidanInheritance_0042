a14372f8cc67765b843234a0b841292838a3a0fb
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:26:06 +0630

    create main function and function call , pointer inside

--- aggregation/main.cpp
+++ aggregation/main.cpp
@@ -3,3 +3,28 @@
 using namespace std;
 #include "child.h"
 #include "mother.h"
+
+int main() {
+    mother* varMother = new mother("dini");
+    mother* varMother2 = new mother("novi");
+    child* varChild1 = new child("tono");
+    child* varChild2 = new child("rini");
+    child* varChild3 = new child("Dewi");
+
+    varMother->addChild(varChild1);
+    varMother->addChild(varChild2);
+    varMother2->addChild(varChild3);
+    varMother2->addChild(varChild1);
+
+    varMother->printChildren();
+    varMother2->printChildren();
+
+    delete varMother;
+    delete varMother2;
+    delete varChild1;
+    delete varChild2;
+    delete varChild3;
+
+    system("pause");
+    return 0;
+}
