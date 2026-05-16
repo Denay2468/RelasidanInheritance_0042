e7ac27c04a0e11fc8d390a883af9de23caec4240
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:37:49 +0630

    create person class and fucntion

--- inheritance/main.cpp
+++ inheritance/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class person {
+public:
+    string name;
+
+    person(string pName) :
+        name(pName) {
+        cout << "person created\n" << endl;
+    }
+    ~person() {
+        cout << "person deleted\n" << endl;
+    }
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+};
