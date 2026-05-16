66a2e0b01744c696742c8ff81d6ef5855dc4ee07
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:43:12 +0630

    create human class and contructor , deconstructor

--- composition/human.h
+++ composition/human.h
@@ -3,3 +3,18 @@
 #include <iostream>
 #include <string>
 #include "heart.h"
+
+class human {
+public:
+    std::string name;
+    heart varHeart;
+
+    human(std::string pName)
+        : name(pName) {
+        std::cout << name << " alive\n";
+    }
+    ~human() {
+        std::cout << name << " dead\n";
+    }
+};
+#endif
