2346ce2d80310cba4ac715c5924f3411efe16d66
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:20:23 +0630

    fix error and library call

--- aggregation/child.h
+++ aggregation/child.h
@@ -2,16 +2,3 @@
 #define CHILD_H
 #include <iostream>
 #include <string>
-using namespace std;
-
-class child {
-public:
-    string name;
-    child(string pName) :name(pName) {
-        cout << "Child \"" << name << "\" exists\n";
-    }
-    ~child() {
-        cout << "Child \"" << name << "\" does not exist\n";
-    }
-};
-#endif
