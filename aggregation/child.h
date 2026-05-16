39795c95afa2fb3c4777788010b9f00971c9d936
Denay2468 <denayaung13@gmail.com>
2026-05-16 10:16:31 +0630

    create child class and code inside

--- aggregation/child.h
+++ aggregation/child.h
@@ -3,3 +3,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class child {
+public:
+    string name;
+    child(string pName) :name(pName) {
+        cout << "Child \"" << name << "\" exists\n";
+    }
+    ~child() {
+        cout << "Child \"" << name << "\" does not exist\n";
+    }
+};
+#endif
